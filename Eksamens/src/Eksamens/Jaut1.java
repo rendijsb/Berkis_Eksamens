948cd4e0dcda8de5c6bc09be7d39d609cd8e03e8
Rendijs <rendijsberkis7@gmail.com>
2023-05-31 21:45:46 +0300

    Pirma jautajuma pabeigsana

--- Eksamens/src/Eksamens/Jaut1.java
+++ Eksamens/src/Eksamens/Jaut1.java
@@ -1,5 +1,6 @@
 package Eksamens;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,42 +11,60 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SakumaEkrans extends JFrame implements ActionListener{
+public class Jaut1 extends JFrame implements ActionListener{
 	JFrame frame;
 	ImageIcon image;
 	JLabel displayField;
-	JButton b1=new JButton("Sākt testu");
-	JButton b2=new JButton("Beigt");
+	JButton b1=new JButton("while(){}do;");
+	JButton b2=new JButton("do{while();");
+	JButton b3=new JButton("do{}while();");
+	JButton b4=new JButton("do{}while;");
 	JPanel panel=new JPanel();
 	String izvele;
-	SakumaEkrans() {
+	Jaut1() {
 		frame = new JFrame();
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-			image = new ImageIcon(getClass().getResource("/bildes/SakumaEkrans.png"));
+			image = new ImageIcon(getClass().getResource("/bildes/Jaut1.png"));
 			displayField = new JLabel(image);
 		
-		  
-        panel.setBounds(55, 577, 315, 113);   
+			panel.setBounds(55, 377, 315, 113);
+
 		     
-        b1.setBounds(55, 577, 315, 113);
+        b1.setBounds(55, 377, 315, 113);
         b1.setOpaque(false);
         b1.setFont(new Font("Arial", Font.ITALIC, 30));
         
        
-        b2.setBounds(425, 577, 315, 113);
+        b2.setBounds(425, 377, 315, 113);
         b2.setOpaque(false);
         b2.setFont(new Font("Arial", Font.ITALIC, 30));
+        
+        b3.setBounds(55, 577, 315, 113);
+        b3.setOpaque(false);
+        b3.setFont(new Font("Arial", Font.ITALIC, 30));
+        
+       
+        b4.setBounds(425, 577, 315, 113);
+        b4.setOpaque(false);
+        b4.setFont(new Font("Arial", Font.ITALIC, 30));
+        
         b1.addActionListener(this);
         b2.addActionListener(this);
+        b3.addActionListener(this);
+        b4.addActionListener(this);
         frame.add(b2);
+        frame.add(b3);
+        frame.add(b4);
         
         
-//        b1.setContentAreaFilled(false);
-//        b1.setBorderPainted(false);
+        b1.setContentAreaFilled(false);
+        b1.setBorderPainted(false);
         
         frame.add(b1);
+      
+        panel.setBackground(Color.RED);
         frame.add(panel);  
         frame.add(displayField);
         frame.setSize(800, 800);
@@ -69,7 +88,7 @@ public class SakumaEkrans extends JFrame implements ActionListener{
 		
 	}
 	public static void main(String[] args) {
-		SakumaEkrans SakumaEkrans = new SakumaEkrans();
+		Jaut1 Jaut1 = new Jaut1();
 		
 		
 	}
--- Eksamens/src/Eksamens/Jaut2.java
+++ Eksamens/src/Eksamens/Jaut2.java
@@ -10,36 +10,52 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SakumaEkrans extends JFrame implements ActionListener{
+public class Jaut1 extends JFrame implements ActionListener{
 	JFrame frame;
 	ImageIcon image;
 	JLabel displayField;
-	JButton b1=new JButton("Sākt testu");
-	JButton b2=new JButton("Beigt");
+	JButton b1=new JButton("while(){}do;");
+	JButton b2=new JButton("do{while();");
+	JButton b3=new JButton("do{}while();");
+	JButton b4=new JButton("do{}while;");
 	JPanel panel=new JPanel();
 	String izvele;
-	SakumaEkrans() {
+	Jaut1() {
 		frame = new JFrame();
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-			image = new ImageIcon(getClass().getResource("/bildes/SakumaEkrans.png"));
+			image = new ImageIcon(getClass().getResource("/bildes/Jaut1.png"));
 			displayField = new JLabel(image);
 		
 		  
-        panel.setBounds(55, 577, 315, 113);   
+
 		     
-        b1.setBounds(55, 577, 315, 113);
+        b1.setBounds(55, 377, 315, 113);
         b1.setOpaque(false);
         b1.setFont(new Font("Arial", Font.ITALIC, 30));
         
        
-        b2.setBounds(425, 577, 315, 113);
+        b2.setBounds(425, 377, 315, 113);
         b2.setOpaque(false);
         b2.setFont(new Font("Arial", Font.ITALIC, 30));
+        
+        b3.setBounds(55, 577, 315, 113);
+        b3.setOpaque(false);
+        b3.setFont(new Font("Arial", Font.ITALIC, 30));
+        
+       
+        b4.setBounds(425, 577, 315, 113);
+        b4.setOpaque(false);
+        b4.setFont(new Font("Arial", Font.ITALIC, 30));
+        
         b1.addActionListener(this);
         b2.addActionListener(this);
+        b3.addActionListener(this);
+        b4.addActionListener(this);
         frame.add(b2);
+        frame.add(b3);
+        frame.add(b4);
         
         
 //        b1.setContentAreaFilled(false);
@@ -69,7 +85,7 @@ public class SakumaEkrans extends JFrame implements ActionListener{
 		
 	}
 	public static void main(String[] args) {
-		SakumaEkrans SakumaEkrans = new SakumaEkrans();
+		Jaut1 Jaut1 = new Jaut1();
 		
 		
 	}
